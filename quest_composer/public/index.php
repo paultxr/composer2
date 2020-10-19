69f135b2774a1ab6de8d065d840bf62d6a47e11f
paultxr <70207932+paultxr@users.noreply.github.com>
2020-10-19 10:40:37 +0200

    Add files via upload

---
+++ quest_composer/public/index.php
@@ -0,0 +1,8 @@
+<?php
+require "../vendor/autoload.php";
+
+echo src\Wcs\Hello::talk();
+
+use HelloWorld\SayHello;
+
+echo HelloWorld\SayHello::world();
---
+++ quest_composer/src/Wcs/Hello.php
@@ -0,0 +1,10 @@
+<?php
+namespace src\Wcs;
+
+class Hello
+{
+    static function talk(): string
+    {
+        return "Hello World";
+    }
+}
